c6cbc3061aa85ef204d76597f9955bf7a00b6c1f
Moath <moath.dlaii@gmail.com>
2020-07-13 16:39:02 -0700

    update route works

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,6 +7,15 @@ class Profile extends React.Component {
   // ==============
   // STATE
   // ==============
+  state = {
+    show:false
+  }
+
+  toggleShow = () => {
+    this.setState({
+        show:!this.state.show
+    })
+  }
 
     render () {
       const {tools,
@@ -22,7 +31,19 @@ class Profile extends React.Component {
             return (
               <>
               <h1>{tool.title} || Price/Day ${tool.price}</h1>
+              <button onClick={this.toggleShow}>Edit Tool</button>
               <button value={tool.id} onClick={deleteTool}>Delete Tool</button>
+              { this.state.show ?
+              <div>
+                <form id={tool.id} onSubmit={updateTool}>
+                  <input onKeyUp={updateTitle} type="text" placeholder="Title" required/><br/>
+                  <input onKeyUp={updateImg} type="text" placeholder="ImageURL" required/><br/>
+                  <input onKeyUp={updateDescription} type="text" placeholder="Description" required/><br/>
+                  <input onKeyUp={updatePrice} type="text" placeholder="Price" required/><br/>
+                  <input onKeyUp={updateTag} type="text" placeholder="Tags" required/><br/>
+                  <input type="submit" value="Update Tool"/>
+                </form>
+              </div>: null }
               </>
             )
 
