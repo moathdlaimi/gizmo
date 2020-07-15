aee48feb5aa1a86c66bb067cebc17a4d8cc80387
Moath <moath.dlaii@gmail.com>
2020-07-15 01:17:35 -0700

    added css to the login/sign forms

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import axios from "axios";
+import axios from "axios"
+import '../css/Login.css'
 
 class Login extends React.Component {
   state = {
@@ -44,17 +45,16 @@ class Login extends React.Component {
 
   render () {
     return (
-      <>
-        <h3>Login</h3>
-        <div>
-          <form onSubmit={this.createSession}>
-            <input  type="text" onChange={this.onChangeUsername} placeholder="Username" required/><br/>
-            <input  type="password" onChange={this.onChangePassword} placeholder="Password" required/><br/>
-            <input type="submit" value="Login"/>
+
+        <div className="login-div">
+        <h1>Login</h1>
+          <form onSubmit={this.createSession} className="login-form">
+            <input  className="login-input" type="text" onChange={this.onChangeUsername} placeholder="Username" required/><br/>
+            <input  className="login-input" type="password" onChange={this.onChangePassword} placeholder="Password" required/><br/>
+            <input className="login-submit-btn" type="submit" value="Login"/>
 
           </form>
-        </div>
-      </>
+      </div>
     )
   }
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import axios from "axios";
+import '../css/Signup.css'
 
 class Signup extends React.Component {
   state = {
     users:[]
   }
-  
+
   createUser = (event) => {
     event.preventDefault();
     axios.post(
@@ -40,18 +41,15 @@ class Signup extends React.Component {
 
   render () {
     return (
-    <div>
-      <h3>Signup</h3>
-
-      <div>
-        <form onSubmit={this.createUser}>
-          <input onKeyUp={this.createName} type="text" placeholder="Username" required/><br/>
-          <input onKeyUp={this.createPassword} type="password" placeholder="Password" required/><br/>
-          <input type="submit" value="Create New User"/>
-
-        </form>
+      <div className="signup-div">
+        <h1>Signup</h1>
+          <form onSubmit={this.createUser} className="signup-form">
+            <input className="signup-input" onKeyUp={this.createName} type="text" placeholder="Username" required/><br/>
+            <input className="signup-input" onKeyUp={this.createPassword} type="password" placeholder="Password" required/><br/>
+            <input className="signup-submit-btn" type="submit" value="Create New User"/>
+          </form>
       </div>
-    </div>
+
     )
   }
 }
