85909a0be2e629d998d58c55509321abd5d0071d
Moath <moath.dlaii@gmail.com>
2020-07-16 11:15:22 -0700

    setting up heroku

--- models/tool.php
+++ models/tool.php
@@ -1,7 +1,25 @@
 <?php
 
+$dbconn = null;
+if(getenv('DATABASE_URL')){
+    $connectionConfig = parse_url(getenv('DATABASE_URL'));
+    $host = $connectionConfig['host'];
+    $user = $connectionConfig['user'];
+    $password = $connectionConfig['pass'];
+    $port = $connectionConfig['port'];
+    $dbname = trim($connectionConfig['path'],'/');
+    $dbconn = pg_connect(
+        "host=".$host." ".
+        "user=".$user." ".
+        "password=".$password." ".
+        "port=".$port." ".
+        "dbname=".$dbname
+    );
+} else {
+  $dbconn = pg_connect("host=localhost dbname=gizmo");
+}
 
-$dbconn = pg_connect("host=localhost dbname=gizmo");
+
 
     class Tool {
       public $id;
